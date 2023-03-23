c29392dadbeaa100b896170e5fac968eb22736bf
BB-Simon <simon.chowdery@gmail.com>
2023-03-24 01:41:30 +0200

    Fix type warning and design

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -11,7 +11,7 @@ const Calculator = () => (
         <Button
           key={name}
           className={`item item-${name}`}
-          title={char}
+          title={char.toString()}
         />
       ))}
     </div>
