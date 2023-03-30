e4c0629392c21f71a40c2c49b7a148aa1b07b4a9
BB-Simon <simon.chowdery@gmail.com>
2023-03-30 23:24:58 +0200

    Add Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,34 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navigation = () => (
   <nav className="nav">
-    nav
+    <Link to="/" className="link">Math Magicians</Link>
+    <ul className="nav-item">
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? 'active' : 'link')}
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/calculator"
+          className={({ isActive }) => (isActive ? 'active' : 'link')}
+        >
+          Calculator
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/quote"
+          className={({ isActive }) => (isActive ? 'active' : 'link')}
+        >
+          Quote
+        </NavLink>
+      </li>
+    </ul>
   </nav>
 );
 
