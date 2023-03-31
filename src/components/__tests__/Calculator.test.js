4a2b6e524879803675cafde82bfe1392fc37d496
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-03-31 21:02:21 +0300

    Add snapshoot testing for Calculator component

--- src/components/__tests__/Calculator.test.js
+++ src/components/__tests__/Calculator.test.js
@@ -1,9 +1,9 @@
 import TestRenderer from 'react-test-renderer';
-import Home from '../Home';
+import Calculator from '../Calculator';
 
 describe('Jest Snapshot testing suite', () => {
   it('renders correctly', () => {
-    const domTree = TestRenderer.create(<Home />).toJSON();
+    const domTree = TestRenderer.create(<Calculator />).toJSON();
     expect(domTree).toMatchSnapshot();
   });
 });
